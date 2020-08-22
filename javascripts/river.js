9d24b87472f077f92a583a50e7b4bd7a99ca5ea5
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-08-22 14:23:54 -0500

    fixed card issue where image height is fixed now

--- javascripts/river.js
+++ javascripts/river.js
@@ -7,7 +7,7 @@
     heyBear.forEach((yourBear) => {
       $("#bear-card").append(`
               <div class="card" style="width: 18rem;">
-              <img src="${yourBear.image}" class="card-img-top" alt="...">
+              <img id="bearPic" src="${yourBear.image}" class="card-img-top" alt="...">
                   <div class="bear-card-body">
                    <p class="card-text">Bear Name: ${yourBear.name}</p>
                   </div>
